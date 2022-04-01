cafef1bed3f0a0ffd626fe64b99f21a85960ebb4
exxnnonymous <sulavbista51@gmail.com>
2022-04-02 02:43:10 +0545

    debugging5 authUser.js

--- pages/api/getUser.js
+++ pages/api/getUser.js
@@ -1,10 +1,12 @@
 import connectDB from "../../helper/connection";
 import User from "../../models/user";
-import { parse } from "cookie";
 import jwt from "jsonwebtoken";
 
 async function getUserById(req, res) {
-    const cookie = parse(req.headers.cookie).auth;
+  console.log(req)
+  if(req.cookies){
+    const cookie = req.cookies.auth;
+    console.log(cookie)
     if (cookie) {
       try {
         const decoded = jwt.verify(cookie, process.env.jwtsecret);
@@ -22,6 +24,11 @@ async function getUserById(req, res) {
     } else {
       return res.status(400).json({ message: "unauthorized" });
     }
+  }else{
+    res.status(500).json({message:"you are not authenticated"})
+  }
+  
+    
 }
 
 export default connectDB(getUserById);
